d04f62a98a44834c8866187641ad1099d560d9ba
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-02-01 23:18:28 +0200

    Update dockerBuildAndPush.groovy

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -1,4 +1,15 @@
 def call(String imageName, String registry, String imageTag) {
     // Your Docker build and push steps here
+     withCredentials([usernamePassword(credentialsId:"${dockerHubCredentialsID}", usernameVariable: 'DOCKER_REGISTRY_USERNAME', passwordVariable: 'DOCKER_REGISTRY_PASSWORD')]) {
+                    script {
+                        
+                            sh "echo \${DOCKER_REGISTRY_PASSWORD} | docker login -u \${DOCKER_REGISTRY_USERNAME} --password-stdin"
+                            sh "docker build -t ${imageNameapp} ."
+                            sh "docker tag ${imageNameapp} ${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${imageTagApp}"
+                            sh "docker push ${DOCKER_REGISTRY}/${DOCKER_IMAGE}:${imageTagApp}"
+                         
+                            // Clean up even if the build or push fails
+                            sh "docker rmi -f ${imageNameapp}"
+                        
     
 }
