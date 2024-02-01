921728363305f418bb2b9da10b74f9d91556dcf1
salma mohamed makram <109488469+Salmamohamedm@users.noreply.github.com>
2024-02-01 23:10:25 +0200

    Create deployToOpenShift.groovy

---
+++ vars/deployToOpenShift.groovy
@@ -0,0 +1,5 @@
+def call(String openshiftSecret, String openshiftServer, String openshiftProject,
+         String appServiceName, String appImage, String appPort) {
+    // Your OpenShift deployment steps here
+    // ...
+}
